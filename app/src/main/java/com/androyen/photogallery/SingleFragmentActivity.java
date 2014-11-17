596a3fe335cb579f2846541516859c9bcf5f61ea
Androyen <androyen@aol.com>
2014-11-17 12:01:39 -0600

    Initial commit

---
+++ app/src/main/java/com/androyen/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package com.androyen.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by rnguyen on 11/5/14.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    //Abstract method to get the current fragment
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        //Add Fragment to Activity
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment =  createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
