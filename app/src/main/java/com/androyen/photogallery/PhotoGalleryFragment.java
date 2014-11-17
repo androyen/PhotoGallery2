8003d42ab9639d74788769ccadab14638694f771
Androyen <androyen@aol.com>
2014-11-17 12:25:37 -0600

    Initial implementation of FetchItemsTask

--- app/src/main/java/com/androyen/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/androyen/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,21 @@
 package com.androyen.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by rnguyen on 11/17/14.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = PhotoGalleryFragment.class.getSimpleName();
 
     GridView mGridView;
 
@@ -20,6 +25,9 @@ public class PhotoGalleryFragment extends Fragment {
 
         //retain the fragment during rotation
         setRetainInstance(true);
+
+        //Execute AsyncTask
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -29,5 +37,23 @@ public class PhotoGalleryFragment extends Fragment {
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
         return v;
+    }
+
+    //AsyncTask to run networking calls
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            }
+            catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+            }
+
+            return null;
+        }
+
     }
 }
